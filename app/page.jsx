90eafe25e1036c3d5242c164bf3212bd8101a6cb
salahelfatimi <salahfatimi16@gmail.com>
2024-12-27 11:09:53 +0100

    refactor: remove trailing slashes from metadata URLs for consistency

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ export async function generateMetadata() {
     openGraph: {
       title: "French O'Paname & Pizzeria & Food & Livraison Rapide",
       description: "Découvrez les meilleures pizzas & Food chez French O'Paname  . Savourez des pizzas & Food fraîches, délicieuses et livrées rapidement . Ouvert de midi à 5h du matin. Commandez dès maintenant !",
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       robots: {
         index: true,
         follow: true,
