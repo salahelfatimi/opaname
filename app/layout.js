1c77a364e5ee4559077b368a994ecc2d3fdb9206
salahelfatimi <salahfatimi16@gmail.com>
2024-12-30 17:36:19 +0100

    refactor: add canonical URL to metadata in layout configuration

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata= {
     title: {  template:" French O'Paname | %s " },
     description: "Découvrez les meilleures pizzas & Food chez French O'Paname  . Savourez des pizzas & Food fraîches, délicieuses et livrées rapidement . Ouvert de midi à 5h du matin. Commandez dès maintenant !",
     metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
+    alternates: {
+      canonical: "/",
+    },
     keywords: ["French O'Paname Marrakech, French O'Paname Casablanca, Livraison plats français Marrakech, Livraison plats français Casablanca, Opaname menu français, Fast-food Marrakech, Fast-food Casablanca, Livraison Glovo cuisine française, Plats français Opaname"],      
  
 }
