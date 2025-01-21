74327e54d4ed8725ecf1cce2313bc4554f862078
salahelfatimi <salahfatimi16@gmail.com>
2025-01-21 15:49:25 +0100

    chore: add blank line for improved readability in WooCommerce API connection file

--- app/api/WooCommerceRestApiConnect.js
+++ app/api/WooCommerceRestApiConnect.js
@@ -4,5 +4,6 @@ export const api = new WooCommerceRestApi({
   url: process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
   consumerKey: process.env.WOO_CONSUMER_KEY,
   consumerSecret: process.env.WOO_CONSUMER_SECRET,
+  
   version: "wc/v3"
 });
