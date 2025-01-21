70cb37b62e23305cbb4fa89c99d5f1d39c64d004
salahelfatimi <salahfatimi16@gmail.com>
2025-01-21 15:44:01 +0100

    chore: remove unnecessary blank line in WooCommerce API connection file

--- app/api/WooCommerceRestApiConnect.js
+++ app/api/WooCommerceRestApiConnect.js
@@ -4,6 +4,5 @@ export const api = new WooCommerceRestApi({
   url: process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
   consumerKey: process.env.WOO_CONSUMER_KEY,
   consumerSecret: process.env.WOO_CONSUMER_SECRET,
-  
   version: "wc/v3"
 });
